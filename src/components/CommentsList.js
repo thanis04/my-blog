f4ec4236b43a685d73e178268cf5bfedbae3f28f
Thanis <mtmorion75@gmail.com>
2024-09-12 10:10:21 +0400

    Intigrated succesfully via GET

---
+++ src/components/CommentsList.js
@@ -0,0 +1,13 @@
+const CommmentsList = ({ comments }) => (
+    <>
+        <h3>Comments:</h3>
+        {comments.map(comment => (
+        <div className="comment" key={comment.postedBy+': '+comment.text}>
+            <h4>{comment.postedBy}</h4>
+            <p>{comment.text}</p>
+        </div>
+        ))}
+    </>
+);
+
+export default CommmentsList;
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,27 @@
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from './NotFoundPage';
+import CommmentsList from "../components/CommentsList";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      console.log(response);
+      const newArticleInfo = response.data;
+      console.log(response.data);
+      setArticleInfo(newArticleInfo);
+    }
+
+    loadArticleInfo();
+  }, []);
+
+ 
   const article = articles.find(article => article.name === articleId);
 
   if (!article) {
@@ -13,9 +31,11 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
+      <CommmentsList comments={articleInfo.comments} />
     </>
   );
 };
