b668c2e79236e540211d5d9bc730e55d5c4cb79c
Thanis <mtmorion75@gmail.com>
2024-09-24 19:41:42 +0400

    All development completed

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -7,26 +7,33 @@ import axios from "axios";
 import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
-  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [], canUpvote: false });
+  const { canUpvote } = articleInfo;
   const { articleId } = useParams();
 
   const [user, isLoading] = useState();
 
   useEffect(() => {
     const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`);
+      const token = user && await user.getIdToken();
+      const headers = token ? { authToken: token } : {};
+      const response = await axios.get(`/api/articles/${articleId}`, { headers });
       const newArticleInfo = response.data;
       setArticleInfo(newArticleInfo);
     }
 
-    loadArticleInfo();
-  }, []);
+    if (isLoading) {
+      loadArticleInfo();
+    }
+  }, [isLoading, user]);
 
  
   const article = articles.find(article => article.name === articleId);
 
   const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const token = user && await user.getIdToken();
+    const headers = token ? { authToken: token } : {};
+    const response = await axios.put(`/api/articles/${articleId}/upvote`,null, { headers });
     const updateArticle = response.data;
     setArticleInfo(updateArticle);
   }
@@ -40,7 +47,7 @@ const ArticlePage = () => {
       <h1>{article.title}</h1>
       <div className="upvotes-section">
         {user 
-          ? <button onClick={addUpvote}>Upvote</button>
+          ? <button onClick={addUpvote}>{canUpvote ? 'Upvote': 'Already Upvoted'}</button>
           : <button>Log into upvote</button>
         }  
         <p>This article has {articleInfo.upvotes} upvote(s)</p>
@@ -50,9 +57,8 @@ const ArticlePage = () => {
       ))}
       {user
         ? <AddCommentForm
-          articleName={articleId}
-          onArticleUpdated={updateArticle => setArticleInfo(updateArticle)} 
-          />
+            articleName={articleId}
+            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
         : <button>Log into add a comment</button>}
       <CommmentsList comments={articleInfo.comments} />
     </>
